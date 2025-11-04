1d832f16c0efb9d6fc3486707679eb79130734e8
Meerzulee <17146298+meerzulee@users.noreply.github.com>
2025-11-04 18:16:24 +0600

    add scripts: db-reset and bin/ze  and rubocop

--- template.rb
+++ template.rb
@@ -1,6 +1,10 @@
 # Jumbo Rails Template
 # Usage: rails new {project_name} --database=postgresql --skip-javascript -m=template.rb
 
+def source_paths
+  [__dir__]
+end
+
 def add_gems
   # Inertia.js for building modern single-page apps
   gem 'inertia_rails', '~> 3.0'
@@ -42,12 +46,33 @@ def setup_procfile
   end
 end
 
+def copy_rubocop_config
+  say 'Copying RuboCop configuration...', :blue
+  copy_file '.rubocop.yml', '.rubocop.yml', force: true
+end
+
+def setup_zellij
+  say 'Setting up Zellij configuration...', :blue
+  directory '.zellij', '.zellij'
+  copy_file 'bin/ze', 'bin/ze', force: true
+  chmod 'bin/ze', 0755
+end
+
+def setup_bin_scripts
+  say 'Setting up custom bin scripts...', :blue
+  copy_file 'bin/db-reset', 'bin/db-reset', force: true
+  chmod 'bin/db-reset', 0755
+end
+
 def main
   add_gems
 
   after_bundle do
     setup_inertia
     setup_procfile
+    copy_rubocop_config
+    setup_zellij
+    setup_bin_scripts
 
     say
     say 'Jumbo template successfully applied!', :green
